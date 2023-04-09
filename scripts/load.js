e6ecd4140b48ff22bb3e6ef5f9e5971a7de9e784
Drakuzz19 <126423050+Drakuzz19@users.noreply.github.com>
2023-04-08 21:04:48 -0600

    js de pantalla de carga

---
+++ scripts/load.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', function(){
+    let loaderWindow = document.getElementsByClassName("loader-1")[0];
+    console.log(loaderWindow+"Hola");
+    loaderWindow.classList.add("loader-2");
+    
+
+});
