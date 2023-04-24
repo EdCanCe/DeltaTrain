5b7c6fc307e980bc3af5e49f0390201fab357338
Drakuzz19 <drakuzz19@gmail.com>
2023-04-23 21:36:50 -0600

    imagen pantalla completa

---
+++ scripts/image.js
@@ -0,0 +1,23 @@
+let imagenes = document.querySelectorAll('img');
+
+for(let i=0; i<imagenes.length; i++){
+    imagenes[i].addEventListener("click", function(){
+        
+        let pantallaCompleta = document.createElement('div');
+        
+        pantallaCompleta.classList.add('img-extended-container')
+
+        let imagenAmpliada= new Image();
+        imagenAmpliada.src=this.src;
+
+        pantallaCompleta.appendChild(imagenAmpliada);
+
+        document.body.appendChild(pantallaCompleta);
+
+        pantallaCompleta.addEventListener('click', function(){
+            document.body.removeChild(pantallaCompleta);
+        })
+
+    }
+    );
+}
