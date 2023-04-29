eae29c3afa5ee5231126358168794a8502093b58
EdCanCe <edcance.dev@gmail.com>
2023-04-29 01:01:25 -0600

    recipe sin imágen ni visualización, pero ya subida

--- recipecreator.php
+++ recipecreator.php
@@ -28,12 +28,22 @@ $portion = str_replace('|', '', $portion); #Esta parte crea la receta como tal
 $protein = str_replace('|', '', $protein);
 $fat = str_replace('|', '', $fat);
 $carbs = str_replace('|', '', $carbs);
-$portionSend=$portion."|".$protein."|".$fat."|".$carbs;
+$unique = time();
+$portionSend=$portion."|".$protein."|".$fat."|".$carbs."|".$unique;
 $query = "INSERT INTO Recipe(FKID_User_Recipe, Name_Recipe, Preparation_Recipe, Portions_Recipe) VALUES ($CurrentUserID, '$name', '$preparation', '$portionSend')";
 $result = mysqli_query($conexion, $query);
 
 
-$query = "SELECT * FROM Recipe WHERE FKID_User_Recipe = $CurrentUserID AND Name_Recipe = '$name' AND Preparation_Recipe = '$preparation' AND Portions_Recipe = '$portionSend'"; #Esta parte crea los ingredientes por receta
+$query = "SELECT * FROM Recipe WHERE FKID_User_Recipe = $CurrentUserID AND Name_Recipe = '$name' AND Preparation_Recipe = '$preparation' AND Portions_Recipe = '$portionSend'"; #Esta parte te da el ID de la receta que se acaba de hacer
 $result = mysqli_query($conexion, $query);
 $row = mysqli_fetch_row($result);
 $idRecipe = $row[0];
+
+for($i=0;$i<count($ingredient)-1;$i++){ #Esta parte crea los ingredientes por receta
+    $query = "SELECT ID_Ingredient FROM Ingredient where Name_Ingredient = '".$ingredient[$i]."'";
+    $result = mysqli_query($conexion, $query);
+    while($row=mysqli_fetch_assoc($result)){
+        $query = "INSERT INTO RecipeIngredient(FKID_Recipe_RecipeIngredient, FKID_Ingredient_RecipeIngredient) VALUES ($idRecipe, ".$row['ID_Ingredient'].")";
+        $result2 = mysqli_query($conexion, $query);
+    }
+}
